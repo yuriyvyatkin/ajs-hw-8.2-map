68d153f167d3acff7fa66b7301dfc4fb3b18c986
Yuriy Vyatkin <yuriyvyatkin@yandex.ru>
2021-07-04 15:26:02 +1000

    Format code in basic.test.js

--- src/js/__tests__/basic.test.js
+++ src/js/__tests__/basic.test.js
@@ -1,6 +1,6 @@
 import ErrorRepository from '../ErrorRepository';
 
-test('Class "ErrorRepository" exists', () => {
+test('class "ErrorRepository" exists', () => {
   expect(new ErrorRepository()).toBeDefined();
 });
 
@@ -15,8 +15,12 @@ errorRepository.errors.set(1, 'Описание ошибки с кодом 1');
 
 test('class "ErrorRepository" has a working "translate" method', () => {
   expect(typeof errorRepository.translate).toBe('function');
+
   let output = errorRepository.translate(1);
+
   expect(output).toBe('Описание ошибки с кодом 1');
+
   output = errorRepository.translate(2);
+
   expect(output).toBe('Unknown error');
 });
